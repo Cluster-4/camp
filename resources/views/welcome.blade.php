331dcfce0c3fd8d8fe3cb2c65dbf87c2a4c28f04
Nack233 <65160233@go.buu.ac.th>
2024-04-01 21:41:31 +0700

    65160233
    ggez

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -832,7 +832,7 @@
 </head>
 
 <body class="antialiased">
-    <h1>Welcome to cluster 4</h1>
+    <h1>Welcome to cluster 44444444444444444444444444444444444</h1>
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
