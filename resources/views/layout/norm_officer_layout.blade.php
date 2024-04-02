8051a13e5309a8056be1df4a8fb461d6b5256a4e
Copter0v0 <88174531+Copter0v0@users.noreply.github.com>
2024-04-02 14:31:04 +0700

    bootstrap

---
+++ resources/views/layout/norm_officer_layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="{{ url('dist/css/bootstrap.min.css') }}">
+        <script src="{{ url('dist/js/boostrap.min.js') }}"></script>
+    </head>
+    <body>
+        <div class=""></div>
+    </body>
+</html>
