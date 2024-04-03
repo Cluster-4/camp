4612613a41b540df929243c6de12ec0a2690c86b
Copter0v0 <88174531+Copter0v0@users.noreply.github.com>
2024-04-03 23:37:08 +0700

    sidebar V1.0.1

--- resources/views/layout/norm_officer_layout.blade.php
+++ resources/views/layout/norm_officer_layout.blade.php
@@ -11,20 +11,27 @@
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Noto+Sans+Thai:wght@100..900&display=swap" rel="stylesheet">
         <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/2.2.0/uicons-solid-rounded/css/uicons-solid-rounded.css'>
     </head>
+    <style>
+        .navbar-nav .active {
+            color: red;
+        }
+    </style>
     <body style="font-family: Noto Sans Thai, DM Sans;">
         <div class="d-flex flex-row">
-            <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 350px; height: 100vh" >
+            {{-- sidebar --}}
+            <div class="d-flex flex-column flex-shrink-0 p-3 text-white" style="width: 350; height: 100vh; background-color: #242038" >
                 <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto mx-auto text-white text-decoration-none">
                     <img
                         src="logo_bmrs.jpg"
-                        class="img-fluid rounded"
+                        class="img-fluid"
                         alt=""
                         width="73px"
                         height="65.39px"
+                        style="border-radius: 15px"
                     />
                     <span class="fs-5 lh-sm ms-3">ระบบจองห้องประชุม<br>หน่วยงานราชการ</span>
                 </a>
-                <hr>
+                <hr class="invisible">
                 <ul class="nav nav-pills flex-column mb-auto">
                     <li class="nav-item">
                         <a href="#" class="d-flex nav-link active align-items-center" aria-current="booking room" >
@@ -40,17 +47,20 @@
                     </li>
                 </ul>
                 <hr>
-                <div class="d-flex align-items-center justify-content-between ">
+                <div class="d-flex align-items-center justify-content-between">
                     <a href="#" class="d-flex align-items-center text-white text-decoration-none" aria-expanded="false">
                         <img src="https://media.discordapp.net/attachments/1209200207963758673/1224255943861080064/a8a6659eb448af110e0eb9cfb83e901d.png?ex=661cd432&is=660a5f32&hm=a4a235e24939438515f682e285e9e1fe9d10fb1ea91b0bf873690fee774c090b&=&format=webp&quality=lossless&width=628&height=585" alt="" width="32" height="32" class="rounded-circle me-2">
                         <strong><u>เจ้าหน้าที่</u></strong>
                     </a>
-                    <i class="fi fi-sr-sign-out-alt" style="font-size: 30px"></i>
+                    <a href="{{ url('/') }}" class="nav-link text-white"><i class="fi fi-sr-sign-out-alt" style="font-size: 30"></i></a>
                 </div>
             </div>
-            <div clas>
-                <h1 class="display-3">@yield('content')</h1>
-
+            <div class="d-flex flex-row mx-auto mt-1">
+                {{-- navbar --}}
+                <div class="d-flex bg-dark text-white align-items-center p-4 fs-3" style="width: 80vw; height: 10%; background-image: linear-gradient(to right ,#1E1450 ,#1E3ACC); border-radius: 18px;">
+                    <i class="fi fi-sr-meeting-alt me-4" style="font-size: 45px;"></i>
+                    จองห้องประชุม
+                </div>
             </div>
         </div>
     </body>
