a65a895382bbcc6b4e6845774e4615bf5711c482
uyariht <nest.thirayu@gmail.com>
2024-04-05 20:39:48 +0700

    Update homev2.blade.php

--- resources/views/homev2.blade.php
+++ resources/views/homev2.blade.php
@@ -1,5 +1,3 @@
-@extends()
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -36,33 +34,85 @@
             <h1 style="margin-left: 1rem;">ระบบจองห้องประชุมหน่วยงานรายการ</h1>
         </div>
         <!-- กรอบใหญ่ -->
-        <div class="shadow mt-3 mx-auto position-relative" style="width: 95%; height: 85%; border-radius: 15px; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+        <div class="shadow mt-3 mx-auto position-relative"
+            style="width: 95%; height: 85%; border-radius: 15px; display: flex; flex-direction: column; justify-content: center; align-items: center;">
             <!-- ลงชื่อเข้าใช้ -->
             <div class="position-absolute top-0 end-0" style="margin-top: 1rem; margin-right: 1rem; font-size:30px;">
                 ลงชื่อเข้าใช้
             </div>
             <!-- เมนู -->
-            <div class="shadow row" style="width: 60%; height: 10%; border-radius: 15px; background-color: #D9D9D9; display: flex; justify-content: center; align-items: center;">
+            <div class="shadow row"
+                style="width: 60%; height: 10%; border-radius: 15px; background-color: #D9D9D9; display: flex; justify-content: center; align-items: center;">
                 <!-- จองห้อง -->
-                <div class="col-6" id="booking" style="color:#000C6A; border-right: 1px solid #C2C2C2;" onclick="goBookingPage()">
+                <div class="col-6" id="booking" style="color:#000C6A; border-right: 1px solid #C2C2C2;"
+                    onclick="goBookingPage()">
                     <div class="row" style="display: flex; justify-content: center;">
-                        <div class="col-10" style="padding:2%; border-bottom: 1px solid #000C6A; font-size: 30px; font-weight:600; display: flex; justify-content: center; align-items: center;">
+                        <div class="col-10"
+                            style="padding:2%; border-bottom: 1px solid #000C6A; font-size: 30px; font-weight:600; display: flex; justify-content: center; align-items: center;">
                             จองห้องประชุม
                         </div>
                     </div>
                 </div>
                 <!-- ติดตามสถานะการจอง -->
-                <div class="col-6" id="tracking" style="color:#5F5F5F;" onclick="goTrackingPage()" onmouseover="this.style.color='#000C6A'" onmouseout="this.style.color='#5F5F5F'">
+                <div class="col-6" id="tracking">
                     <div class="row" style="display: flex; justify-content: center;">
-                        <div class="col-10" style="padding:2%; font-size: 30px; font-weight:600; display: flex; justify-content: center; align-items: center;">
-                            ติดตามสถานะการจอง
+                        <div class="col-10"
+                            style="padding:2%; font-size: 30px; font-weight:600; display: flex; justify-content: center; align-items: center;">
+                            <a href="#" class="nav-link" style="color: #5F5F5F" onclick="goTrackingPage()"
+                                onmouseover="this.style.color='#000C6A'" onmouseout="this.style.color='#5F5F5F'">
+                                ติดตามสถานะการจอง</a>
                         </div>
                     </div>
                 </div>
             </div>
             <!-- กรอบ input ข้อมูล -->
-            <div class="shadow mt-3" style="width: 60%; height: 60%; border-radius: 15px; background-color: #D9D9D9; display: flex; justify-content: center; align-items: center;">
-                
+            <div class="shadow mt-3"
+                style="width: 60%; height: 60%; border-radius: 15px; background-color: #D9D9D9;justify-content: center; align-items: center;">
+                <form>
+                    <!-- ชื่อห้อง -->
+                    <div class="row" style="display: flex; justify-content: center;">
+                        <div class="col-10 mt-5">
+                            <input type="text" class="form-control"
+                                style="height: 8vh; border-radius: 15px; display: flex; justify-content: center; align-items: center; font-size: 20px;"
+                                id="name_room" placeholder="ค้นหาห้องประชุม">
+                        </div>
+                    </div>
+                    <!-- วันที่ใช้ห้อง -->
+                    <div class="row" style="display: flex; justify-content: center;">
+                        <div class="col-5 mt-2">
+                            <input required="" type="text" id="date_in" class="form-control" style="height: 8vh; border-radius: 15px; font-size: 20px;"
+                            placeholder="วันที่เข้าใช้" onfocus="(this.type='date')" />
+                        </div>
+                        <div class="col-5 mt-2">
+                            <input required="" type="text" id="date_in" class="form-control" style="height: 8vh; border-radius: 15px; font-size: 20px;"
+                            placeholder="วันที่สิ้นสุดการใช้" onfocus="(this.type='date')" />
+                        </div>
+                    </div>
+                    <!-- เวลาที่ใช้ห้อง -->
+                    <div class="row" style="display: flex; justify-content: center;">
+                        <div class="col-5 mt-2">
+                            <input required="" type="text" id="time_in" class="form-control" style="height: 8vh; border-radius: 15px; font-size: 20px;"
+                            placeholder="เวลาเข้าใช้" onfocus="(this.type='time')" />
+                        </div>
+                        <div class="col-5 mt-2">
+                            <input required="" type="text" id="time_in" class="form-control" style="height: 8vh; border-radius: 15px; font-size: 20px;"
+                            placeholder="เวลาที่สิ้นสุดการใช้" onfocus="(this.type='time')" />
+                        </div>
+                    </div>
+                    <!-- ปุ่มค้นหา -->
+                    <div class="row">
+                        <div class="col-12 mt-5" style="display: flex; justify-content: center; align-items: center;">
+                            <button type="submit" style="width: 30%; height: 8vh; border-radius: 15px; font-size: 20px;
+                            background-color:#000C6A; color:white; box-shadow: 0 5px 10px #00000080;">
+                                ค้นหา</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- หมายเหตุ -->
+            <div class="mt-3" style="font-size:20px; display: flex; justify-content: center; align-items: center;">
+                <p><span style="color:red;">หมายเหตุ</span> : กรุณากรอกในช่องที่มีเครื่องหมาย
+                <span style="color:red;"> *</span> ให้ครบทุกช่อง</p>
             </div>
         </div>
     </div>
