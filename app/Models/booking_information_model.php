87e79adaf4c1bcd5128716d5f639a0d499a102c7
Nack233 <65160233@go.buu.ac.th>
2024-04-08 23:33:27 +0700

    bkg

---
+++ app/Models/booking_information_model.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class booking_information_model extends Model
+{
+    protected $table = 'booking_information';
+    protected $primaryKey = 'bkg_id';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'bkg_tracking_pin',
+        'bkg_status',
+        'bkg_fname',
+        'bkg_lname',
+        'bkg_tel',
+        'bkg_date_use',
+        'bkg_date_end',
+        'bkg_create_time',
+        'bkg_cancel_resson',
+        'bkg_approve_date',
+        'bkg_rm_id',
+        'bkg_acc_id',
+        'bkg_acc_appv_id'
+    ];
+
+    protected $dates = [
+        'bkg_date_use',
+        'bkg_date_end',
+        'bkg_create_time',
+        'bkg_approve_date'
+    ];
+}
