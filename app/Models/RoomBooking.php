420189f9d4b8f28cc6fcdaa162d8bf9824caa26c
Nack233 <65160233@go.buu.ac.th>
2024-04-08 14:42:58 +0700

    ok

--- app/Models/RoomBooking.php
+++ app/Models/RoomBooking.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RoomBooking extends Model
 {
-    use HasFactory;
+    protected $table = 'room_bookings';
+    protected $fillable = ['user_name', 'room_name', 'start_date', 'end_date', 'start_time', 'end_time'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RoomBookingController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
