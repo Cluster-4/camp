126dc6f7f2445f5924d79efc0ac0daa95ce744d0
uyariht <nest.thirayu@gmail.com>
2024-04-10 11:01:27 +0700

    Update process_second.blade.php

--- resources/views/other/process_second.blade.php
+++ resources/views/other/process_second.blade.php
@@ -35,8 +35,12 @@
         function showSuccessMessage() {
             let randomCode = generateRandomCode(6);
             let message =
+<<<<<<< Updated upstream
                 "หมายเลขการจอง :" + randomCode +
                 "\nหมายเหตุ : หมายเลขการจองสามารถนำไปตรวจสอบสถานะการจองได้ในเมนูติดตามสถานะการจอง";
+=======
+                "หมายเลขการจอง :" + randomCode + "\nหมายเหตุ : หมายเลขการจองสามารถนำไปตรวจสอบสถานะการจองได้ในเมนูติดตามสถานะการจอง";
+>>>>>>> Stashed changes
             message = message.replaceAll("\n", "<br>");
             Swal.fire({
                 title: "จองเสร็จสิ้น",
@@ -82,6 +86,7 @@
         </div>
         <p style="font-size:350%; color: #000C6A; ">กรอกรายละเอียดการจอง</p>
         <div class="mt-2 row d-flex align-item-center justify-content-center" style="width:50%; height:50%;">
+<<<<<<< Updated upstream
             <form method="POST" action="{{ route('store_booking') }}">
                 @csrf
                 <div class="shadow  mt-2"
@@ -115,8 +120,41 @@
                     </div>
                     <div class="d-flex justify-content-center mt-5">
                         <div class="g-recaptcha" data-sitekey="6LdCH7MpAAAAACPV2aZa_XrzCqL4a1aB1bDUKzqr"></div>
+=======
+            <div class="shadow  mt-2"
+                style="background-color: #c7c7c7; border-radius: 18px; font-size: 150%; width:100%; margin-bottom:1%;  padding:5%;">
+                <div class="form-group row mb-3 mt-3">
+                    <div class="col-3">
+                        <label for="f_name" class="card-text me-2">ชื่อ :</label>
+                    </div>
+                    <div class="col-9">
+                        <input type="text" class="shadow form-control" id="f_name" placeholder="กรอกชื่อจริง"
+                            style="width: 100%; border: none; margin-left:auto; font-size: 100%">
+>>>>>>> Stashed changes
                     </div>
                 </div>
+                <div class="form-group row mb-3">
+                    <div class="col-3">
+                        <label for="surname" class="card-text me-1">นามสกุล :</label>
+                    </div>
+                    <div class="col-9">
+                        <input type="text" class="shadow form-control" id="l_name" placeholder="กรอกนามสกุล"
+                            style="width: 100%; border: none; margin-left:auto; font-size: 100%">
+                    </div>
+                </div>
+                <div class="form-group row mb-3">
+                    <div class="col-3">
+                        <label for="phone" class="card-text me-2">เบอร์โทรศัพท์ :</label>
+                    </div>
+                    <div class="col-9">
+                        <input type="text" class="shadow form-control" id="phone" placeholder="xxx-xxxx-xxx"
+                            style="width: 100%; height:130%; border: none; margin-left:auto;">
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center mt-5">
+                    <div class="g-recaptcha" data-sitekey="6LdCH7MpAAAAACPV2aZa_XrzCqL4a1aB1bDUKzqr"></div>
+                </div>
+            </div>
         </div>
         <div class="d-flex justify-content-end " style="width: 87%; height: 7%; margin-top: 7rem">
             <button class="btn btn-secondary me-2" style="width: 20%; font-size:20px;"
